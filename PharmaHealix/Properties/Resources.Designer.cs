5f7c2de9cb504f9daef15f2abd48c5307ec61422
Baba-Yaga10 <saabidsaleh3214000@gmail.com>
2026-05-07 23:00:14 +0600

    doctor menu updated by some buttons

--- PharmaHealix/Properties/Resources.Designer.cs
+++ PharmaHealix/Properties/Resources.Designer.cs
@@ -213,6 +213,16 @@ namespace PharmaHealix.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap rsz_1calendar_12212473 {
+            get {
+                object obj = ResourceManager.GetObject("rsz_1calendar_12212473", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap rsz_capsule_16305123 {
             get {
                 object obj = ResourceManager.GetObject("rsz_capsule_16305123", resourceCulture);
@@ -256,6 +266,16 @@ namespace PharmaHealix.Properties {
         internal static System.Drawing.Bitmap rsz_medicine_11109007 {
             get {
                 object obj = ResourceManager.GetObject("rsz_medicine_11109007", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap rsz_planning_8169817 {
+            get {
+                object obj = ResourceManager.GetObject("rsz_planning_8169817", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }

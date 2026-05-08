57c9244c9356daec8edc2723c4dbd9b9a555a343
samia126 <aktersamia09876@gmail.com>
2026-05-09 01:18:02 +0600

    Initial project upload

---
+++ PharmaHealix/AdminInventoryForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PharmaHealix
+{
+    public partial class AdminInventoryForm : Form
+    {
+        public AdminInventoryForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUser_Click(object sender, EventArgs e)
+        {
+            AdminUserForm form = new AdminUserForm();
+            form.Show();
+            this.Hide();
+        }
+
+        private void btnPatient_Click(object sender, EventArgs e)
+        {
+            AdminPatientForm form = new AdminPatientForm();
+            form.Show();
+            this.Hide();
+        }
+
+        private void btnInventory_Click(object sender, EventArgs e)
+        {
+            // Already on Inventory page
+        }
+
+        private void btnAppointment_Click(object sender, EventArgs e)
+        {
+            AdminAppointmentForm form = new AdminAppointmentForm();
+            form.Show();
+            this.Hide();
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+    }
+}
---
+++ PharmaHealix/AdminUserForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PharmaHealix
+{
+    public partial class AdminUserForm : Form
+    {
+        public AdminUserForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUser_Click(object sender, EventArgs e)
+        {
+            // Already on User page
+        }
+
+        private void btnPatient_Click(object sender, EventArgs e)
+        {
+            AdminPatientForm form = new AdminPatientForm();
+            form.Show();
+            this.Hide();
+        }
+
+        private void btnInventory_Click(object sender, EventArgs e)
+        {
+            AdminPatientForm form = new AdminPatientForm();
+            form.Show();
+            this.Hide();
+        }
+
+        private void btnAppointment_Click(object sender, EventArgs e)
+        {
+            AdminAppointmentForm form = new AdminAppointmentForm();
+            form.Show();
+            this.Hide();
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AdminUserForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
